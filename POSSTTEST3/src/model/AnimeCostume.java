b06cc6aa471854c0ddcdc0955a27bc18304b6ac3
jaakeee <162022730+jaakeee@users.noreply.github.com>
2025-04-05 19:02:25 +0800

    Add files via upload

---
+++ POSSTTEST3/src/model/AnimeCostume.java
@@ -0,0 +1,33 @@
+package model;
+
+public class AnimeCostume extends Costume {
+    private String characterName;
+    private String animeTitle;
+
+    public AnimeCostume(int id, String name, String size, double price, String characterName, String animeTitle) {
+        super(id, name, size, price);
+        this.characterName = characterName;
+        this.animeTitle = animeTitle;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public String getAnimeTitle() {
+        return animeTitle;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public void setAnimeTitle(String animeTitle) {
+        this.animeTitle = animeTitle;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Karakter: " + characterName + " | Anime: " + animeTitle;
+    }
+}
---
+++ POSSTTEST3/src/model/Costume.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Costume {
+    private int id;
+    private String name;
+    private String size;
+    private double price;
+
+    public Costume(int id, String name, String size, double price) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return "ID: " + id + " | Nama: " + name + " | Ukuran: " + size + " | Harga: " + price;
+    }
+}
